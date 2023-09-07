ffca487f775896933168bcef92cd669824ff01ce
ANASSMRHANENE <mrhanene@gmail.com>
2023-09-06 17:33:00 -0700

    my commit

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def magic_calculation(m, n, o):
+    """Match bytecode provided by Holberton School."""
+    if m < n:
+        return (o)
+    if o > n:
+        return (m + n)
+    return (m*n - o)
