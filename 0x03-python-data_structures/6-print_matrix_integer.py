6aa670c4e4ed579eb3e71e8601a7f599f0aee8d8
ANASSMRHANENE <mrhanene@gmail.com>
2023-09-11 16:00:45 -0700

    my commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix):
-    for i in matrix:
-        for n in i:
-            print("{:d}".format(n), end="\t")
+    for row in matrix:
+        for num in row:
+            print("{:d}".format(num), end="\t")
         print()
