d3f6b01210f379cc2f2956d65fefd99ab8dfddd3
ANASSMRHANENE <mrhanene@gmail.com>
2023-09-11 15:29:49 -0700

    my commit

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    nlist = my_list.copy()
-    if abs(idx) >= len(my_list):
-        nlist[idx] = element
+    if idx < -len(my_list) or idx >= len(my_list):
+        return my_list.copy()
+
+    new_list = my_list[:]
+    new_list[idx] = element
+
     return new_list
