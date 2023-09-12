54a8d39b27d19036f64c4686a04ff128e8417811
ANASSMRHANENE <mrhanene@gmail.com>
2023-09-12 16:09:14 -0700

    my commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
-
-num_args = len(argv) - 1
-
-if num_args == 0:
-    print("0 arguments.")
-else:
-    print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
-
-    for i, arg in enumerate(argv[1:], start=1):
-        print(f"{i}: {arg}")
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    else:
+        print("{:d} argument".format(len(sys.argv) - 1), end="")
+        if len(sys.argv) > 2:
+            print("s", end="")
+        print(":")
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
