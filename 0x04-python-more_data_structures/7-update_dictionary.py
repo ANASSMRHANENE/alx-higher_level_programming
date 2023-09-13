277da998e813bbb80484b4391e3ad1847d2eb599
ANASSMRHANENE <mrhanene@gmail.com>
2023-09-13 16:58:32 -0700

    my commit

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -5,5 +5,5 @@ def update_dictionary(a_dictionary, key, value):
     else:
         for j in a_dictionary:
             if j == key:
-                a_dictionary[i] = value
+                a_dictionary[j] = value
     return a_dictionary
