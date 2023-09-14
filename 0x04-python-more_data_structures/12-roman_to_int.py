953fa7d9372847ecb272fa70101296218d3b7328
ANASSMRHANENE <mrhanene@gmail.com>
2023-09-13 17:12:53 -0700

    my commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,8 +5,8 @@ def roman_to_int(roman_string):
     roman_o = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     roman_t = 0
     for j in range(len(roman_string)):
-        if j > 0 and roman_o[roman_string[j]] > roman_d[roman_string[j - 1]]:
-            roman_n += roman_o[roman_string[j]] - 2 * \
+        if j > 0 and roman_o[roman_string[j]] > roman_o[roman_string[j - 1]]:
+            roman_t += roman_o[roman_string[j]] - 2 * \
                         roman_o[roman_string[j - 1]]
         else:
             roman_t += roman_o[roman_string[j]]
