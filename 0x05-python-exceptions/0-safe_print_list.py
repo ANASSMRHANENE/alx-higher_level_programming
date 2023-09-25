40f10b1ab42426442d3a25fc61d2e5da4fd808d2
ANASSMRHANENE <mrhanene@gmail.com>
2023-09-25 15:25:05 -0700

    my commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
     pr = 0
     for j in range(0, x):
         try:
-            print("{}".format(my_list[i]), end="")
+            print("{}".format(my_list[j]), end="")
             pr += 1
         except:
             continue
