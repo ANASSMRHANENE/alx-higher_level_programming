8969f45ee81dd156d00a27c5d263d9e9db59cccf
ANASSMRHANENE <mrhanene@gmail.com>
2023-09-25 15:38:39 -0700

    my commit

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -19,5 +19,5 @@ def list_division(my_list_1, my_list_2, list_length):
             r = 0
             print("out of range")
         finally:
-            _list.append(result)
+            _list.append(r)
     return _list
