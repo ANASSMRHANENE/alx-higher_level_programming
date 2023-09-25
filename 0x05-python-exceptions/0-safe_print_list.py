246610d577fea6caa1cf67b7277bc8c45a04f4a0
ANASSMRHANENE <mrhanene@gmail.com>
2023-09-25 15:20:10 -0700

    my commit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-safe_print_list = __import__('0-safe_print_list').safe_print_list
 
-my_list = [1, 2, 3, 4, 5]
 
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+def safe_print_list(my_list=[], x=0):
+    j = 0
+    pr = 0
+    for j in range(0, x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            pr += 1
+        except:
+            continue
+    print()
+    return pr
