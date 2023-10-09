c9084e8589cecd6c3b0c3ba648fd4975201d1835
ANASSMRHANENE <mrhanene@gmail.com>
2023-10-09 16:02:08 -0700

    my commit

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" 0x0A. task 8 """
+""" 0x0A. Python - Inheritance, task 8 """
 
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Inherits from Rectangle and thus from BaseGeometry
+    """Inherits from Rectangle and thus from BaseGeometry, for use with
+    rectangular constructs of equal sides.
 
     Args:
         size (int): length of side of square
@@ -23,7 +24,8 @@ class Square(Rectangle):
     def area(self):
         """Returns square area as size * size.
 
-        * Required by task instructions
+        * Required by task instructions, but overloading Rectangle.area() is
+        redundant here, as it has the same result.
 
         Attributes:
             __size (int): length of side of square
