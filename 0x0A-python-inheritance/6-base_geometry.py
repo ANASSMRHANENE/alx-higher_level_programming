18587f96931a127d1bbfaedeb87b62ad6bfc10da
ANASSMRHANENE <mrhanene@gmail.com>
2023-10-09 15:47:11 -0700

    my commit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" 0x0A. task 6 """
+
+
+class BaseGeometry:
+    """empty area() method.
+
+    """
+    def area(self):
+        """only raises exception to notify user.
+
+        """
+        raise Exception('area() is not implemented')
