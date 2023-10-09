365fd993217b72bdbcd7b7f97d5d327f47dfdfb6
ANASSMRHANENE <mrhanene@gmail.com>
2023-10-09 16:03:50 -0700

    my commit

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" 0x0A.  task 8 """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Inherits from Rectangle and thus from BaseGeometry
+
+    Args:
+        size (int): length of side of square
+
+    Attributes:
+        __size (int): length of side of square
+
+    """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Returns square area as size * size
+        Attributes:
+            __size (int): length of side of square
+
+        Returns:
+            __size ** 2
+
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """ Provides printable representation of Square object.
+
+        """
+        return '[Square] {}/{}'.format(self.__size, self.__size)
