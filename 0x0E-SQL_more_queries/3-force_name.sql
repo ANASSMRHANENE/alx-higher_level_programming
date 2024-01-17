eaacf6991db1934045b195e58ac152b7931fdc83
ANASS MRHANENE <mrhanene@gmail.com>
2024-01-17 13:43:12 -0800

    my commit

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,23 +1,5 @@
-SET @db_name = ?;
-
--- Check if the table exists in the specified database
-SELECT COUNT(*) INTO @table_exists
-FROM information_schema.tables
-WHERE table_schema = @db_name AND table_name = 'force_name';
-
--- Create the table only if it does not exist
-SET @create_table_query = IF(@table_exists = 0,
-    CONCAT('
-        USE ', @db_name, ';
-        CREATE TABLE force_name (
-            id INT,
-            name VARCHAR(256) NOT NULL
-        );
-    '),
-    'SELECT ''Table force_name already exists.'';'
+-- Creates the table force_name.
+CREATE TABLE IF NOT EXISTS `force_name` (
+    `id`   INT,
+    `name` VARCHAR(256) NOT NULL
 );
-
--- Execute the query to create the table
-PREPARE create_table_stmt FROM @create_table_query;
-EXECUTE create_table_stmt;
-DEALLOCATE PREPARE create_table_stmt;
